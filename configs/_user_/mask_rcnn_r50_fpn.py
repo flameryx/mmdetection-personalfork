602c4f8f90aa6aa2dc5bd46029bfa47b65785a7c
lorenz <lorenz.hufe@web.de>
2023-03-08 22:13:17 +0100

    added mark rcnn config - not workin atm

---
+++ configs/_user_/mask_rcnn_r50_fpn.py
@@ -0,0 +1,42 @@
+_base_ = '../mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py'
+
+
+# Change the number of classes
+model = dict(
+    roi_head=dict(
+        mask_head=dict(num_classes=1),
+        bbox_head=dict(num_classes=1)
+    )
+)
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('object',)
+
+metainfo = {
+    'CLASSES': ('object', ),
+    'PALETTE': [
+        (220, 20, 60),
+    ]
+}
+
+
+data = dict(
+    workers_per_gpu=0,
+    samples_per_gpu=4,
+    train=dict(
+        img_prefix='/data/input/train/',
+        classes=classes,
+        ann_file='/data/input/train/annotation_coco.json'),
+    val=dict(
+        img_prefix='/data/input/val/',
+        classes=classes,
+        ann_file='/data/input/val/annotation_coco.json'),
+    test=dict(
+        img_prefix='/data/input/val/',
+        classes=classes,
+        ann_file='/data/input/val/annotation_coco.json')
+)
+
+work_dir = '/data/output'
+runner = dict(type='EpochBasedRunner', max_epochs=5)
