09012c7dd1b082f54679e93e4dd90ad3237fcbe1
Ricardo Sosa Melo <r.sosamelo@gmail.com>
2023-05-06 17:20:58 +0200

    commit

---
+++ inference/inference.py
@@ -0,0 +1,18 @@
+from mmdet.apis import init_detector, inference_detector
+
+#TODO: Take the NN model name as input when starting the container
+#config_file and checkpoint_file paths will depend of this name
+
+# Specify the path to model config and checkpoint file
+config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+checkpoint_file = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+
+# Build the model from a config file and a checkpoint file
+model = init_detector(config_file, checkpoint_file)
+
+#TODO: Take the image file as input when starting the container
+# Test a single image and show the results
+img = 'images/dogpark.jpg'
+result = inference_detector(model, img)
+
+model.show_result(img, result, out_file='output.png')
