5312401d7d414bf022ddeea1c5da44b00f2aebe1
Ricardo Sosa Melo <r.sosamelo@gmail.com>
2023-05-06 19:03:57 +0200

    fixed paths in inference.py

--- inference/inference.py
+++ inference/inference.py
@@ -1,18 +1,24 @@
 from mmdet.apis import init_detector, inference_detector
+import os
 
-#TODO: Take the NN model name as input when starting the container
-#config_file and checkpoint_file paths will depend of this name
+# Paths
+work_path = '/mmdetection/inference'
+checkpoint_path = f'{work_path}/nn_checkpoint'
+img_path = f'{work_path}/image'
+output_path = f'{work_path}/output'
 
-# Specify the path to model config and checkpoint file
+# Get the nn_checkpoint loaded as volume 
+nn_checkpoint_name = os.listdir(checkpoint_path)[0]
+checkpoint_file = f'{checkpoint_path}/{nn_checkpoint_name}'
 config_file = '/mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
-checkpoint_file = '/mmdetection/inference/nn-checkpoint/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
 
 # Build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file)
 
-#TODO: Take the image file as input when starting the container
-# Test a single image and show the results
-img = '/mmdetection/inference/image/dogpark.jpg'
-result = inference_detector(model, img)
+# Get the image loaded as volume
+img_name = os.listdir(img_path)[0]
+img_file = f'{img_path}/{img_name}'
 
-model.show_result(img, result, out_file='/mmdetection/inference/output/output.png')
+# Test a single image and show the results
+result = inference_detector(model, img_path)
+model.show_result(img_file, result, out_file=f'{output_path}/output.png')
