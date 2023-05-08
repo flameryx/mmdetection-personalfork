93acc3359c0e08ccd48f3de53efb7ece013650ec
Ricardo Sosa Melo <r.sosamelo@gmail.com>
2023-05-08 18:14:20 +0200

    Finished algorithm to search for config file from checkpoint file name

--- inference/inference.py
+++ inference/inference.py
@@ -1,45 +1,122 @@
 from mmdet.apis import init_detector, inference_detector
 import os
 
-# Paths
-work_path = '/mmdetection/inference'
-configs_path = '/mmdetection/configs'
-checkpoint_path = f'{work_path}/nn-checkpoint'
-img_path = f'{work_path}/image'
-output_path = f'{work_path}/output'
+# Version 1
+# Searches for all folders and files in /mmdetection/configs for a file that matches
+# the name of the checkpoint file, ensuring that the adecuate configuration is used
+# for the given NN.
+# Works if the checkpoint file has the exact same name as the confi file
+def GetConfigFile1(configs_path, nn_checkpoint_name):
+  # Splits the file name in name and extension
+  nn_checkpoint_name = os.path.splitext(nn_checkpoint_name)[0]
 
-# Get the nn_checkpoint loaded as volume 
-nn_checkpoint_name = os.listdir(checkpoint_path)[0]
-checkpoint_file = f'{checkpoint_path}/{nn_checkpoint_name}'
-
-# Version 1: Checkpoint file has the exact same name as the config
-# Find the .config file that corresponds to the NN name given in the checkpoint
-# Iterate through all folders in configs
-
-# Removes the file extension from the checkpoint filename
-nn_checkpoint_name = os.path.splitext(nn_checkpoint_name).stem
-
-for folder in os.listdir(configs_path):
-    if os.path.isdir(f'{configs_path}/{folder}'):
-      for file_name in folder:
-         if nn_checkpoint_name in file_name:
-            print(f'{configs_path}/{folder}/{file_name}')
+  match_path = ""
+  found_match = False
+  # Iterates through all files in config folder
+  for folder in os.listdir(configs_path):
+      folder_path = f'{configs_path}/{folder}'
+      # Continues if it is a folder that is not _user_
+      if os.path.isdir(folder_path) and folder != '_user_':
+        # Iterates through all the files in the folder
+        for file_name in os.listdir(folder_path):
+          # Splits the file name in name and extension
+          file_name_short, file_name_ext = os.path.splitext(file_name)
+          # Continues if it is a python file whose entire name is in the checkpoint's name
+          if file_name_ext == ".py" and file_name_short == nn_checkpoint_name:
+              match_path = f'{folder_path}/{file_name}'
+              found_match = True
+              break
         
-        
-  # Iterate through all files in each folder
+        if found_match:
+           break
+  
+  return match_path;
 
-# Version 2: Checkpoint file has additional ID added after the NN name
+def BuildPartialName(splitted_name, last_index):
+  builded_name = ""
+  for idy in range(last_index + 1):
+    builded_name += splitted_name[idy]
+
+    if idy != last_index:
+      builded_name += "_"
+  
+  return builded_name
 
 
-config_file = '/mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 
-# Build the model from a config file and a checkpoint file
-model = init_detector(config_file, checkpoint_file)
+# Version 2
+# Searches for all folders and files in /mmdetection/configs for a file that matches
+# the name of the checkpoint file, ensuring that the adecuate configuration is used
+# for the given NN. It breaks down the name by the delimeter "_" and searches for matches up to
+# a given part of the name.
+# Works even when there is some identification data after the name of the NN in the checkpoint
+# file name. Drawback is that it is not so precise, since it chooses the last match of the last 
+# builded name. In some cases this may not be the correct configuration
+def GetConfigFile2(configs_path, nn_checkpoint_name):
+  # Splits the file name in name and extension
+  nn_checkpoint_name = os.path.splitext(nn_checkpoint_name)[0]
 
-# Get the image loaded as volume
-img_name = os.listdir(img_path)[0]
-img_file = f'{img_path}/{img_name}'
+  splitted_checkpoint_name = nn_checkpoint_name.split('_')
 
-# Test a single image and show the results
-result = inference_detector(model, img_file)
-model.show_result(img_file, result, out_file=f'{output_path}/output.png')
+  last_match_path = ""
+
+  for idx, name_part in enumerate(splitted_checkpoint_name):
+    found_match = False
+
+    builded_name = BuildPartialName(splitted_checkpoint_name, idx)
+
+    # Iterates through all files in config folder
+    for folder in os.listdir(configs_path):
+        folder_path = f'{configs_path}/{folder}'
+        # Continues if it is a folder that is not _user_
+        if os.path.isdir(folder_path) and folder != '_user_':
+          # Iterates through all the files in the folder
+          for file_name in os.listdir(folder_path):
+            # Splits the file name in name and extension
+            file_name_short, file_name_ext = os.path.splitext(file_name)
+            # Continues if it is a python file whose entire name is in the checkpoint's name
+            if file_name_ext == ".py" and builded_name in file_name_short:
+                last_match_path = f'{folder_path}/{file_name}'
+                found_match = True
+                break
+          
+          if found_match:
+             break
+    
+    if found_match:
+       continue
+    else:    
+      break
+  
+  return(last_match_path)
+
+
+def main():
+  # Paths
+  work_path = '/mmdetection/inference'
+  configs_path = '/mmdetection/configs'
+  checkpoint_path = f'{work_path}/nn-checkpoint'
+  img_path = f'{work_path}/image'
+  output_path = f'{work_path}/output'
+
+  # Get the nn_checkpoint loaded as volume 
+  nn_checkpoint_name = os.listdir(checkpoint_path)[0]
+  checkpoint_file = f'{checkpoint_path}/{nn_checkpoint_name}'
+
+  # Get the config file by extracting the name of the NN from the checkpoint filename
+  config_file = GetConfigFile1(configs_path, nn_checkpoint_name)
+  if not config_file:
+    config_file = GetConfigFile2(configs_path, nn_checkpoint_name)
+
+  # Build the model from a config file and a checkpoint file
+  model = init_detector(config_file, checkpoint_file)
+
+  # Get the image loaded as volume
+  img_name = os.listdir(img_path)[0]
+  img_file = f'{img_path}/{img_name}'
+
+  # Test a single image and show the results
+  result = inference_detector(model, img_file)
+  model.show_result(img_file, result, out_file=f'{output_path}/output.png')
+
+main()
--- inference/test.py
+++ inference/test.py
@@ -1,15 +1,101 @@
-from mmdet.apis import init_detector, inference_detector
 import os
+
+
+# Version 1
+# Searches for all folders and files in /mmdetection/configs for a file that matches
+# the name of the checkpoint file, ensuring that the adecuate configuration is used
+# for the given NN.
+# Works if the checkpoint file has the exact same name as the confi file
+def GetConfigFile1(configs_path, nn_checkpoint_name):
+  # Splits the file name in name and extension
+  nn_checkpoint_name = os.path.splitext(nn_checkpoint_name)[0]
+
+  match_path = ""
+  found_match = False
+  # Iterates through all files in config folder
+  for folder in os.listdir(configs_path):
+      folder_path = f'{configs_path}/{folder}'
+      # Continues if it is a folder that is not _user_
+      if os.path.isdir(folder_path) and folder != '_user_':
+        # Iterates through all the files in the folder
+        for file_name in os.listdir(folder_path):
+          # Splits the file name in name and extension
+          file_name_short, file_name_ext = os.path.splitext(file_name)
+          # Continues if it is a python file whose entire name is in the checkpoint's name
+          if file_name_ext == ".py" and file_name_short == nn_checkpoint_name:
+              match_path = f'{folder_path}/{file_name}'
+              found_match = True
+              break
+        
+        if found_match:
+           break
+  
+  return match_path;
+
+def BuildPartialName(splitted_name, last_index):
+  builded_name = ""
+  for idy in range(last_index + 1):
+    builded_name += splitted_name[idy]
+
+    if idy != last_index:
+      builded_name += "_"
+  
+  return builded_name
+
+
+
+# Version 2
+# Searches for all folders and files in /mmdetection/configs for a file that matches
+# the name of the checkpoint file, ensuring that the adecuate configuration is used
+# for the given NN. It breaks down the name by the delimeter "_" and searches for matches up to
+# a given part of the name.
+# Works even when there is some identification data after the name of the NN in the checkpoint
+# file name. Drawback is that it is not so precise, since it chooses the last match of the last 
+# builded name. In some cases this may not be the correct configuration
+def GetConfigFile2(configs_path, nn_checkpoint_name):
+  # Splits the file name in name and extension
+  nn_checkpoint_name = os.path.splitext(nn_checkpoint_name)[0]
+
+  splitted_checkpoint_name = nn_checkpoint_name.split('_')
+
+  last_match_path = ""
+
+  for idx, name_part in enumerate(splitted_checkpoint_name):
+    found_match = False
+
+    builded_name = BuildPartialName(splitted_checkpoint_name, idx)
+
+    # Iterates through all files in config folder
+    for folder in os.listdir(configs_path):
+        folder_path = f'{configs_path}/{folder}'
+        # Continues if it is a folder that is not _user_
+        if os.path.isdir(folder_path) and folder != '_user_':
+          # Iterates through all the files in the folder
+          for file_name in os.listdir(folder_path):
+            # Splits the file name in name and extension
+            file_name_short, file_name_ext = os.path.splitext(file_name)
+            # Continues if it is a python file whose entire name is in the checkpoint's name
+            if file_name_ext == ".py" and builded_name in file_name_short:
+                last_match_path = f'{folder_path}/{file_name}'
+                found_match = True
+                break
+          
+          if found_match:
+             break
+    
+    if found_match:
+       continue
+    else:    
+      break
+  
+  return(last_match_path)
+
 
 configs_path = '/mmdetection/configs'
 nn_checkpoint_name = 'faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+nn_checkpoint_name = 'faster_rcnn_r50_fpn_1x_coco.pth'
 
-nn_checkpoint_name = os.path.splitext(nn_checkpoint_name).stem
-print(nn_checkpoint_name)
-
-for folder in os.listdir(configs_path):
-    if os.path.isdir(f'{configs_path}/{folder}'):
-      for file_name in folder:
-         file_name_short = os.path.splitext(nn_checkpoint_name).stem
-         if file_name_short in nn_checkpoint_name:
-            print(f'{configs_path}/{folder}/{file_name}')
+print("GetConfigFile1 : ")
+print(GetConfigFile1(configs_path, nn_checkpoint_name))
+print("GetConfigFile2 : ")
+print(GetConfigFile2(configs_path, nn_checkpoint_name))
